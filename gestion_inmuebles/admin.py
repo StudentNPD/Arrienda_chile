35c68590fa580485fe38161170e33d6c9f8215a2
Fish Nat <studyestudiante@gmail.com>
2024-11-20 17:50:39 -0300

    Hito 1 realizado

--- gestion_inmuebles/admin.py
+++ gestion_inmuebles/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
-from .models import Inmueble
+from .models import Inmueble, Usuario, RegistroArriendo, UsuarioInmuebles
 
 # Register your models here.
 admin.site.register(Inmueble)
+admin.site.register(Usuario)
+admin.site.register(RegistroArriendo)
+admin.site.register(UsuarioInmuebles)
--- gestion_inmuebles/models.py
+++ gestion_inmuebles/models.py
@@ -1,7 +1,13 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
 class Inmueble(models.Model):
+    TIPOS_INMUEBLE = [
+        ('casa', 'Casa'),
+        ('departamento', 'Departamento'),
+        ('parcela', 'Parcela')
+    ]
+
     nombre = models.CharField(max_length=200)
     descripcion = models.TextField()
     m2_construidos = models.FloatField()
@@ -11,8 +17,90 @@ class Inmueble(models.Model):
     banos = models.IntegerField()
     direccion = models.CharField(max_length=200)
     comuna = models.CharField(max_length=100)
-    tipo_inmueble = models.CharField(max_length=50)
-    precio_mensual = models.DecimalField(max_digits=10, decimal_places=2)
-    
+    tipo_inmueble = models.CharField(
+        max_length=50, 
+        choices=TIPOS_INMUEBLE
+    )
+    precio_mensual = models.IntegerField()
+   
     def __str__(self):
         return self.nombre
+
+class Usuario(models.Model):
+    TIPOS_USUARIO = [
+        ('arrendatario', 'Arrendatario'),
+        ('arrendador', 'Arrendador')
+    ]
+
+    nombre = models.CharField(max_length=200)
+    apellido = models.CharField(max_length=200)
+    rut = models.CharField(max_length=12, unique=True)
+    direccion = models.TextField()
+    telefono = models.CharField(max_length=15)
+    email = models.CharField(max_length=100, unique=True)
+    tipo_usuario = models.CharField(
+        max_length=20, 
+        choices=TIPOS_USUARIO
+    )
+   
+    def __str__(self):
+        return self.nombre
+
+class RegistroArriendo(models.Model):
+    ESTADOS_ARRIENDO = [
+        ('pendiente', 'Pendiente'),
+        ('en_proceso', 'En Proceso'),
+        ('aceptado', 'Aceptado'),
+        ('cancelado', 'Cancelado'),
+        ('finalizado', 'Finalizado')
+    ]
+
+    usuario_arrendador = models.ForeignKey(
+        Usuario, 
+        on_delete=models.CASCADE, 
+        related_name='arriendos_ofrecidos'
+    )
+    usuario_arrendatario = models.ForeignKey(
+        Usuario, 
+        on_delete=models.CASCADE, 
+        related_name='arriendos_solicitados'
+    )
+    inmueble = models.ForeignKey(
+        Inmueble, 
+        on_delete=models.CASCADE
+    )
+    estado = models.CharField(
+        max_length=20, 
+        choices=ESTADOS_ARRIENDO, 
+        default='pendiente'
+    )
+    fecha_inicio = models.DateField(
+        null=True, 
+        blank=True
+    )
+    fecha_fin = models.DateField(
+        null=True, 
+        blank=True
+    )
+    fecha_solicitud = models.DateTimeField(
+        default=timezone.now
+    )
+    
+    def __str__(self):
+        return self.inmueble.nombre
+
+class UsuarioInmuebles(models.Model):
+    inmueble = models.ForeignKey(
+        Inmueble, 
+        on_delete=models.CASCADE
+    )
+    usuario_arrendador = models.ForeignKey(
+        Usuario, 
+        on_delete=models.CASCADE
+    )
+    
+    class Meta:
+        unique_together = ('inmueble', 'usuario_arrendador')
+    
+    def __str__(self):
+        return self.usuario_arrendador.nombre
